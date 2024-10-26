0eb6390b17138edd35c5d99de3e173189df1ed85
Maros Tomasov <maros3845@gmail.com>
2024-10-26 16:57:05 +0200

    working qrcode reading and web scraping

--- Verification/Qrcode.py
+++ Verification/Qrcode.py
@@ -1,14 +1,22 @@
 import cv2
 
 class Qrcode():
-    def __init__(self, filename):
-        self.filename = filename
-        self.image = cv2.imread(filename)
+    def __init__(self):
+        pass        
     
-    def read(self):
+    def read(self, filename=None):
+        if filename is None:
+            return None
+        image = cv2.imread(filename)
         detector = cv2.QRCodeDetector()
-        data, vertices_array, binary_qrcode = detector.detectAndDecode(self.image)
+        data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
+        
+        if not data:
+            inverted_image = self.invert_image(image)
+            data, vertices_array, binary_qrcode = detector.detectAndDecode(inverted_image) 
+
         if vertices_array is not None:
             return data
-        else:
-            return None
+        return None
+    def invert_image(self, image):
+        return cv2.bitwise_not(image)
