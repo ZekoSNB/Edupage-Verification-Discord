ef5889f1d208d2c394c6a1b02588df511aabb6b4
Samuel Siman <samuelsiman7@gmail.com>
2024-10-26 22:23:06 +0200

    new json file and changes in bot

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -1,12 +1,11 @@
 import discord
 import json
 
-
 class Bot():
     def __init__(self):
         intents = discord.Intents.default()
         intents.members = True
-        intents.messages_content = True
+        # intents.messages_content = True
         self.client = discord.Client(intents = intents)
         self.client.event(self.on_ready)
         self.client.event(self.on_member_join)
@@ -16,23 +15,22 @@ class Bot():
         with open('/home/samino/Documents/Edupage-Verification-Discord/data/data.json', 'r') as file:
             return json.load(file)['BOT_TOKEN']
         
+    def get_wellcome_msg(self):
+        with open('/home/samino/Documents/Edupage-Verification-Discord/Bot/msgs.json', 'r') as file:
+            return json.load(file)['WELLCOME_MSG']
     
     async def on_ready(self):
         print(f'We have logged in as {self.client.user}')
 
     async def on_member_join(self, member):
-        print("sending...")
+        print(f"Sending message to {member}...")
         try:
-            await member.send(f'Welcome to the server {member}')
-            print(f"message sent to {member}")
+            await member.send(self.get_wellcome_msg())
+            print(f"Message sent to {member}")
         except discord.Forbidden:
             print(f'Could not send a message to {member}')
         except Exception as e:
-            print(f'An error occured: {e}')
-
-    
-
-
+            print(f'An error occured: {e}') 
 
 if __name__ == '__main__':
     Bot()
