f929855d6bd4e311eba2f775df76c90c2b9e5b8c
Samuel Siman <samuelsiman7@gmail.com>
2024-10-27 21:00:16 +0100

    added commetns and gitignor modified

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -4,30 +4,41 @@ import json
 import aiohttp
 
 class Bot():
+
+    #initialize bot
     def __init__(self):
         self.BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
         self.bot_data = self.get_bot_data()
+
+        #giving bot permissions
         intents = discord.Intents.default()
         intents.members = True
         intents.message_content = True
         self.client = discord.Client(intents = intents)
+
+        #looking for specific events and running bot with token
         self.client.event(self.on_ready)
         self.client.event(self.on_member_join)
-        self.client.run(self.get_token())
         self.client.event(self.on_message)
+        self.client.run(self.get_token())
 
+
+    #get needed information from .json
     def get_bot_data(self):
         with open(os.path.join(self.BASE_DIR, 'Bot', 'Bot_data.json'), 'r') as file:
             return json.load(file)
 
+    #get bot token from .json file
     def get_token(self):
         file_path = os.path.join(self.BASE_DIR, 'data', 'data.json')
         with open(file_path, 'r') as file:
             return json.load(file)['BOT_TOKEN']
     
+    #check if bot is online
     async def on_ready(self):
         print(f'We have logged in as {self.client.user}')
 
+    #detect member join and send welcome mesage
     async def on_member_join(self, member):
 
         WELCOME_MSG = self.bot_data['WELCOME_MSG']
@@ -44,17 +55,17 @@ class Bot():
         except Exception as e:
             print(f'An error occured: {e}') 
 
-    #receive message
+    #detect message receive
     async def on_message(self, message):
         if message.author == self.client.user:
             return
 
-        #only if attachments from DM
+        #check if DM attachment
         if isinstance(message.channel, discord.DMChannel) and message.attachments:
             print(f'{message.author} sent a message: {message.content}, {len(message.attachments)} attachments available')
             await message.channel.send(f"Sprava '{message.content}' prijata, {len(message.attachments)} obrazkov.")
            
-           #download images
+            #check sent images
             for attachment in message.attachments:
                 if attachment.content_type and 'image' in attachment.content_type:
                     print(f"Image '{attachment.filename}' is downloading.")
