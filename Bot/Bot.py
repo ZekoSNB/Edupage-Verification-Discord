d0ed6a306ce0fd241de95049b1fe639ffa7fa4d0
Samuel Siman <samuelsiman7@gmail.com>
2024-10-27 17:07:45 +0100

    functioning welcome message

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -1,7 +1,9 @@
 import discord
 import json
 
+
 class Bot():
+
     def __init__(self):
         intents = discord.Intents.default()
         intents.members = True
@@ -11,34 +13,37 @@ class Bot():
         self.client.event(self.on_member_join)
         self.client.run(self.get_token())
 
-
     def get_token(self):
         with open('/home/samino/Documents/Edupage-Verification-Discord/data/data.json', 'r') as file:
             return json.load(file)['BOT_TOKEN']
         
-    def get_wellcome_msg(self):
+    def get_welcome_msg(self):
         with open('/home/samino/Documents/Edupage-Verification-Discord/Bot/msgs.json', 'r') as file:
             return json.load(file)['WELLCOME_MSG']
         
-    def get_tpc_img(self):
-        return('/home/samino/Documents/Edupage-Verification-Discord/Bot/IMG_3118.JPG')
-
-    def get_tmp_img(self):
-        return('/home/samino/Documents/Edupage-Verification-Discord/Bot/IMG_3118.JPG')
+    def get_imgs(self, x):
+        if x == 1:
+            return('/home/samino/Documents/Edupage-Verification-Discord/Bot/EduTMP.jpeg')
+        elif x == 0:
+            return('/home/samino/Documents/Edupage-Verification-Discord/Bot/EduTPC.jpeg')
     
     async def on_ready(self):
         print(f'We have logged in as {self.client.user}')
 
     async def on_member_join(self, member):
+        WELCOME_MSG = self.get_welcome_msg()
+        PC_MSG = "Ak overenie robíš na PC/Laptope: :computer:"
+        MOBILE_MSG = "Ak overenie robíš na mobile: :iphone:"
         print(f"Sending message to {member}...")
         try:
-            await member.send(self.get_wellcome_msg())
-            await member.send(file = discord.File(self.get_img()))
+            await member.send(f"{WELCOME_MSG} \n\n {PC_MSG}", file = discord.File(self.get_imgs(0))) 
+            await member.send(f"{MOBILE_MSG}", file = discord.File(self.get_imgs(1)))
             print(f"Message sent to {member}")
         except discord.Forbidden:
             print(f'Could not send a message to {member}')
         except Exception as e:
             print(f'An error occured: {e}') 
+
 
 if __name__ == '__main__':
     Bot()
