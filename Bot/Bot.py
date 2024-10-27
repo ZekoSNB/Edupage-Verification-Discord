aff128bbd5e01ab236e9aa7047e1f039b526094f
Samuel Siman <samuelsiman7@gmail.com>
2024-10-27 16:45:44 +0100

    functioning bot and welcome mesage

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -11,6 +11,7 @@ class Bot():
         self.client.event(self.on_member_join)
         self.client.run(self.get_token())
 
+
     def get_token(self):
         with open('/home/samino/Documents/Edupage-Verification-Discord/data/data.json', 'r') as file:
             return json.load(file)['BOT_TOKEN']
@@ -18,6 +19,12 @@ class Bot():
     def get_wellcome_msg(self):
         with open('/home/samino/Documents/Edupage-Verification-Discord/Bot/msgs.json', 'r') as file:
             return json.load(file)['WELLCOME_MSG']
+        
+    def get_tpc_img(self):
+        return('/home/samino/Documents/Edupage-Verification-Discord/Bot/IMG_3118.JPG')
+
+    def get_tmp_img(self):
+        return('/home/samino/Documents/Edupage-Verification-Discord/Bot/IMG_3118.JPG')
     
     async def on_ready(self):
         print(f'We have logged in as {self.client.user}')
@@ -26,6 +33,7 @@ class Bot():
         print(f"Sending message to {member}...")
         try:
             await member.send(self.get_wellcome_msg())
+            await member.send(file = discord.File(self.get_img()))
             print(f"Message sent to {member}")
         except discord.Forbidden:
             print(f'Could not send a message to {member}')
