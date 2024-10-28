10508cc13c63c47c0267ef8eff3f09589df9778b
Maros Tomasov <maros3845@gmail.com>
2024-10-28 01:01:47 +0100

    Catching fake QRcodes

--- Verification/Verification.py
+++ Verification/Verification.py
@@ -20,8 +20,14 @@ class Verification():
         Qr_data = Qr.read(image_path)
         if Qr_data:
             student_data = self.verify_web(Qr_data)
-            edu_data = self.edu.check(student_data)
-            return edu_data
+
+            if student_data is None:
+                return {
+                    "STATUS": False,
+                    "ERROR": "Invalid URL"
+                }
+            
+            return self.edu.check(student_data)
         else:
             return {
                 "STATUS": False,
@@ -29,22 +35,25 @@ class Verification():
             } 
     
     def verify_web(self, url: str) -> dict:
-        user_data = {
-            "NAME": "",
-            "YEAR": ""
-        }
-        options = Options()
-        options.add_argument('--headless=new')
-        chrome_driver = os.path.join(self.BASE_DIR, 'data', 'chromedriver')
-        service = Service(chrome_driver)
-        driver = webdriver.Chrome(service=service, options=options)
-        driver.get(url)
-        user_data['NAME'] = driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div/div/div[1]/div[6]/div[2]').get_attribute('innerHTML')
-        year = driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div/div/div[1]/div[11]').get_attribute('innerHTML')
-        user_data['YEAR'] = re.sub(r'[\t\n\.]', '', year).split(' ')[::-1][0]
-        driver.quit()
-        
-        return user_data
+        try:
+            user_data = {
+                "NAME": "",
+                "YEAR": ""
+            }
+            options = Options()
+            options.add_argument('--headless=new')
+            chrome_driver = os.path.join(self.BASE_DIR, 'data', 'chromedriver')
+            service = Service(chrome_driver)
+            driver = webdriver.Chrome(service=service, options=options)
+            driver.get(url)
+            user_data['NAME'] = driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div/div/div[1]/div[6]/div[2]').get_attribute('innerHTML')
+            year = driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div/div/div[1]/div[11]').get_attribute('innerHTML')
+            user_data['YEAR'] = re.sub(r'[\t\n\.]', '', year).split(' ')[::-1][0]
+            driver.quit()
+            
+            return user_data
+        except Exception as e:
+            return None
 
     def get_data(self) -> dict:
         with open(os.path.join(self.BASE_DIR, 'data', 'data.json'), 'r') as f:
